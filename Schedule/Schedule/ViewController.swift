18bfa8578645c29c0ad71561b621df50425d9b91
Alberto <musicalalberto@gmail.com>
2019-10-25 23:01:30 -0700

    finished assignment (as well as extra work)

--- Schedule/Schedule/ViewController.swift
+++ Schedule/Schedule/ViewController.swift
@@ -6,6 +6,11 @@
 //  Copyright © 2019 Alberto Mancarella. All rights reserved.
 //
 
+
+
+//Did extra work: finished ENTIRE code, not just assignment -> "Display afternoon classes"
+
+
 import UIKit
 
 class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate{
@@ -30,16 +35,7 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
                         "Tuesday": "Keyboard",
                         "Wednesday": "Music",  "Thursday": "Life Skills",
                             "Friday": "Free Period"]
-    
 
-    
-    
-    let whattime : [String:String] =
-        ["C1":"8:15-9:10", "C2": "9:15-10:10", "C3": "10:30-11:25", "C4":"11:30-12:25", "C5": "1:10-3:00"]
-    
-
-    var time = 3
-    
     @IBOutlet weak var button: UIButton!
     
     @IBOutlet weak var pickerView: UIPickerView!
@@ -53,9 +49,6 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
       func numberOfComponents(in pickerView: UIPickerView) -> Int {
           return 3
       }
-      
-
-      
       
       func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
           if component == 0{
@@ -82,10 +75,8 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
                      return schedule[row]
                   }
     }
-      
-      
-      
-      
+
+
       func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int){
           if component == 0{
               curDayType = dayTypes[row]
@@ -104,45 +95,69 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     @IBAction func getSchedule(_
         sender: Any) {
         let dayTypeRow = pickerView.selectedRow(inComponent: 0)
+        let whichdays =
+            pickerView.selectedRow(inComponent:2)
         let ACBDay = dayTypes[dayTypeRow]
-        
+        let differentdaytime = schedule[whichdays]
+
         let periodRotation:[String:[String]] = [
             "A":["A","B","D","E"],
             "C":["C","A","F","D"],
             "B":["B","C","E","F"]
             ]
-        
-        let mySched = getScheduleForDay(periods:periodRotation[ACBDay]!, mySchedule: mySchedule)
+        var scheduletime: [String:[String]] = [ "Regular":["8:15-9:10", "9:15-10:10", "10:30-11:25","11:30-12:25", "1:10-3:00"],
+            
+            "Double":["8:15-9:50","11:10-12:45", "12:45-1:20","1:25-3:00"],
+            
+        "Assembly":["8:15-9:05","9:10-10:00","11:00-11:50","11:55-12:45","1:15-3:00"]
+
+    ]
+        let mySched = getScheduleForDay(periods:periodRotation[ACBDay]!, mySchedule: mySchedule, time: scheduletime[differentdaytime]!)
         label.text = mySched
     }
-            
-    func getScheduleForDay(periods:[String], mySchedule:[String:String]) -> String{
-        var output = ""
-        for period in periods{
-            
-            output += "\(period) - "
-            output += mySchedule[period] ?? "Free Period"
-            output += "\n"
-
-            print(time)
     
+    
+    func getScheduleForDay(periods:[String], mySchedule:[String:String],time:[String]!) -> String{
+        var output = ""
+    
+        
+        var whatday = pickerView.selectedRow(inComponent:2)
+        var getday = (schedule[whatday])
+        
+        
+        if getday == "Double"{
+            
+            var morningclasses = periods[0]
+            var middayclasses = periods[2]
+            
+            output += " \(time[0]): \(morningclasses) "
+            output += mySchedule[morningclasses] ?? "Free Period"
+            output+="\n"
+             output += " \(time[1]): \(middayclasses) "
+            output += mySchedule[middayclasses] ?? "Free Period"
+            output+="\n"
         }
         
+        else{
+            let sequence = zip(time, periods)
+            for (eachtime, period) in sequence{
+                output += " \(eachtime): \(period)  "
+                output +=  mySchedule[period] ?? "Free Period"
+            output += "\n"
+            }
+        }
+        
+    
+        //afternoon arts classes
         var dayoftheweek = pickerView.selectedRow(inComponent: 1)
         var getcurrentday = (weekdays[dayoftheweek])
         var finalafternoonclass = afternoonSchedule[getcurrentday]
-            
-        output += "1:05-3:00: " + finalafternoonclass!
         
+        output +=  " \(time.last!): \(finalafternoonclass!) "
         return output
+    
     }
-    
-    
-
 }
-    
-    
-    
 
         
         
@@ -150,11 +165,3 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
 
       
       
-      
-      
-      
-    
-      
-      
-
-
