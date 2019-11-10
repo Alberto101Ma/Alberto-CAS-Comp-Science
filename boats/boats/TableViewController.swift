65646b2d62f3057c5a2483f2a59bdcbfec1f8514
Alberto101Ma <48220264+Alberto101Ma@users.noreply.github.com>
2019-11-10 11:43:29 -0800

    progress

--- boats/boats/TableViewController.swift
+++ boats/boats/TableViewController.swift
@@ -1,103 +1,107 @@
 //
 //  TableViewController.swift
-//  boats
+//  Boats
 //
-//  Created by Alberto Mancarella on 11/8/19.
+//  Created by Alberto Mancarella on 11/5/19.
 //  Copyright © 2019 Alberto Mancarella. All rights reserved.
 //
 
 import UIKit
 
-class TableViewController: UITableViewController {
-    
-    var boats = ["SS Wizard":  "Rainbow normal 20 unicrons",
-    "Personal Watercraft":   "neon green  small  ok",
-    "SS tipton cruise ship":   "white  big enough for zack and cody 3 woodys"]
-
+class TableViewController: UITableViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
-        var boatsorted = Array(boats.keys).sorted()
+        boatsorder = Array(boats.keys).sorted()
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the fosllowing line to display an Edit button in the navigation bar for this view controller.
+        
+        // Uncomment thef ollowing line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
+    
     // MARK: - Table view data source
-
+    var boats =  ["test":"123","test123":"bob"]
     
     
-    var boatsorted = [String]()
     
     
+    
+    var boatsorder = [String]()
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return boats.count
+        return boatsorder.count
     }
     
     
-    
-    
-
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        let title = boatsorted[indexPath.row]
+        let title = boatsorder[indexPath.row]
         cell.textLabel!.text = title
-
+        
+        
+        // Configure the cell...
+        
         return cell
     }
-
-
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using segue.destination.
+//        // Pass the selected object to the new view controller.
+//        let vc = segue.destination as! ViewController
+//
+//
+//
+//        let indexPath = self.tableView.indexPathForSelectedRow
+//        let word = self.vocabWords[indexPath!.row]
+//        vc.vocabWord = word
+//        vc.vocabDefinition = self.vocab[word]!
+//    }
+    
 }
