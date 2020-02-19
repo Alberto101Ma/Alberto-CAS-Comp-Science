05c6682506a277eed8dcc60b5c87332b9e1a4036
Alberto <48220264+Alberto101Ma@users.noreply.github.com>
2020-02-19 08:32:06 -0800

    changed width/height of button

--- numberGuesser/numberGuesser/GameViewController.swift
+++ numberGuesser/numberGuesser/GameViewController.swift
@@ -14,13 +14,13 @@ class GameViewController: UIViewController {
     var difficulty = String()
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var stackView: UIStackView!
-    let buttonMessage = ButtonMessage()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         addButtons()
-        stackView.frame = CGRect(x: 0, y: 0, width: 200, height: 1500)
-        scrollView.contentSize = CGSize(width: 200, height: 1500)
+        stackView.frame = CGRect(x: 0, y: 0, width: 200, height: checkWhatDifficulty()*100)
+        scrollView.contentSize = CGSize(width: 200, height: checkWhatDifficulty()*100)
     }
     
     func checkWhatDifficulty() -> Int{
@@ -35,16 +35,20 @@ class GameViewController: UIViewController {
         }
     }
     
-    
+    func goBackToMenu(){
+        let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "Menu") as UIViewController
+        self.present(viewController, animated: false, completion: nil)
+    }
     @objc func tooHigh(){
         let alert = UIAlertController(title: "Try again!", message: "Too high!", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Ok", style: .default))
         self.present(alert, animated: true, completion: {})
     }
-
+    
     @objc func justRight(){
-        let alert = UIAlertController(title: "Congratulations!", message: "Just right", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Ok", style: .default))
+        let alert = UIAlertController(title: "Congratulations!", message: "You picked the right answer!", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "New Game", style: .default, handler: {action in self.goBackToMenu()}))
         self.present(alert, animated: true, completion: {})
     }
     
@@ -54,10 +58,9 @@ class GameViewController: UIViewController {
         self.present(alert, animated: true, completion: {})
     }
     
-
+    
     @objc func addButtons(){
         var randomNumber = Int.random(in: 1...checkWhatDifficulty())
-        
         // Do any additional setup after loading the view.
         let buttons = (1...checkWhatDifficulty()).map({ (x) -> UIButton in
             
@@ -66,23 +69,23 @@ class GameViewController: UIViewController {
             newButton.setTitleColor(UIColor(displayP3Red: 0, green: 0, blue: 1, alpha:
                 1), for: .normal)
             if x < randomNumber {
-              buttonMessage.message = "Too low"
+            
                 newButton.addTarget(self, action: #selector(tooLow), for: .touchUpInside)
-              }
-              else if x > randomNumber {
-                  buttonMessage.message = "Too high"
+            }
+            else if x > randomNumber {
+    
                 newButton.addTarget(self, action: #selector(tooHigh), for: .touchUpInside)
-              }
-              else{
-                  buttonMessage.message = "just right"
+            }
+            else{
+            
                 newButton.addTarget(self, action: #selector(justRight), for: .touchUpInside)
-              }
+            }
             return newButton
         })
         for button in buttons{
             manyButtons.addArrangedSubview(button)
             let constraint = NSLayoutConstraint(item: button, attribute: .height, relatedBy: .equal, toItem: buttons[0], attribute: .height, multiplier: 1, constant: 0)
-            
+            button.frame.size = CGSize(width: 20.0, height: 20.0)
             if button != buttons[0] {
                 manyButtons.addConstraint(constraint)
                 
